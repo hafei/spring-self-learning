7912e3d593d503769056369dd375109fc7e410d4
hafei <zhangmengcoding@gmail.com>
2023-05-13 18:34:04 +0800

    Initial commit && three ways to config spring bean

---
+++ src/main/java/com/example/bean/XmlBean.java
@@ -0,0 +1,12 @@
+package com.example.bean;
+
+public class XmlBean {
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String name;
+
+}
