b44153e19493a35c1a618a56dac4635544ad11ee
hafei <zhangmengcoding@gmail.com>
2023-07-13 23:41:05 +0800

    add

--- SpringBean/src/main/java/org/example/config/MyConfig.java
+++ SpringBean/src/main/java/org/example/config/MyConfig.java
@@ -1,15 +1,21 @@
 package org.example.config;
 
 import org.example.bean.JavaBean;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MyConfig {
+public class MyConfig implements BeanNameAware {
     @Bean
     public JavaBean javaBean(){
         JavaBean javaBean = new JavaBean();
         javaBean.setName("javaBean");
         return javaBean;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("MyConfig.setBeanName: " + name);
     }
 }
